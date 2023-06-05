45e569aa6e3309de96616325c0007aba8ff1ae4f
Freddy Monroy <fredydeltoro@gmail.com>
2023-06-05 12:42:22 -0600

    Config of redux dev tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   combineReducers({ ...reducers }),
-  applyMiddleware(thunk),
+  composeEnhancers(applyMiddleware(thunk))
 );
 
 export default store;
